e5b0678b40a165af313d3c57eaf26aefef495264
Flavia Salazar Urzua <flaviasurzua@gmail.com>
2022-02-13 20:28:46 -0600

    Updated the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formateDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElemnt = document.querySelector("#temperature");
   temperatureElemnt.innerHTML = Math.round(response.data.main.temp);
@@ -9,6 +32,8 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formateDate(response.data.dt * 1000);
 }
 
 let apiKey = "f750b0916c260e8ace2bf35a6b03d249";
