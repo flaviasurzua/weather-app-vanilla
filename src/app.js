a009aaa0092bec24c5174afb13b83c2981cb13a8
Flavia Salazar Urzua <flaviasurzua@gmail.com>
2022-02-13 20:06:39 -0600

    making current temperature and city pop up

---
+++ src/app.js
@@ -0,0 +1,17 @@
+function displayTemperature(response) {
+  let temperatureElemnt = document.querySelector("#temperature");
+  temperatureElemnt.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "f750b0916c260e8ace2bf35a6b03d249";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
