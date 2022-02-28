9ca1b1e1974869761622705122289b51ecdf6447
Flavia Salazar Urzua <flaviasurzua@gmail.com>
2022-02-28 00:55:47 -0600

    Javascript Loop for forecast

--- src/app.js
+++ src/app.js
@@ -21,6 +21,35 @@ function formateDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/03d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-minimun">12°</span>
+            <span class="weather-forecast-temperature-maximum">18°</span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function displayTemperature(response) {
   celsiusTemperature = Math.round(response.data.main.temp);
   let temperatureElemnt = document.querySelector("#temperature");
@@ -90,3 +119,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
